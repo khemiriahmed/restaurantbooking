8d074465dd8c2859e3e234853f78f96033c81c32
khemiriahmed <khemiriahmed6@gmail.com>
2026-09-14 14:34:37 +0100

    RestaurantTable

--- backend/src/main/java/com/akhm/restaurantbooking/entity/RestaurantTable.java
+++ backend/src/main/java/com/akhm/restaurantbooking/entity/RestaurantTable.java
@@ -1,4 +1,50 @@
 package com.akhm.restaurantbooking.entity;
 
+import com.akhm.restaurantbooking.enums.RestaurantTableStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(
+        name = "restaurant_tables",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_restaurant_table_number",
+                        columnNames = {"restaurant_id", "table_number"}
+                )
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RestaurantTable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "table_number", nullable = false)
+    private Integer tableNumber;
+
+    @Column(nullable = false)
+    private Integer capacity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    @Builder.Default
+    private RestaurantTableStatus status =
+            RestaurantTableStatus.AVAILABLE;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    @OneToMany(mappedBy = "restaurantTable")
+    @Builder.Default
+    private List<Reservation> reservations = new ArrayList<>();
 }
--- backend/src/main/java/com/akhm/restaurantbooking/entity/Role.java
+++ backend/src/main/java/com/akhm/restaurantbooking/entity/Role.java
@@ -1,4 +1,23 @@
 package com.akhm.restaurantbooking.entity;
 
+import com.akhm.restaurantbooking.enums.RoleName;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true, length = 30)
+    private RoleName name;
 }
