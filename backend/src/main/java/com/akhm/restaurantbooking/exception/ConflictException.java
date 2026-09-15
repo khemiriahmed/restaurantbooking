5a89c856b90707a78d5fcdcf6c21d3a6f15ccb24
khemiriahmed <khemiriahmed6@gmail.com>
2026-09-15 11:09:55 +0100

    ConflictException

--- backend/src/main/java/com/akhm/restaurantbooking/exception/ConflictException.java
+++ backend/src/main/java/com/akhm/restaurantbooking/exception/ConflictException.java
@@ -1,4 +1,8 @@
 package com.akhm.restaurantbooking.exception;
 
-public class ConflictException {
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
 }
