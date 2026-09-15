b8c3f36294f96facb33c641bc64fb3385c06941d
khemiriahmed <khemiriahmed6@gmail.com>
2026-09-15 11:03:33 +0100

    Bad Request

--- backend/src/main/java/com/akhm/restaurantbooking/exception/BadRequestException.java
+++ backend/src/main/java/com/akhm/restaurantbooking/exception/BadRequestException.java
@@ -1,4 +1,8 @@
 package com.akhm.restaurantbooking.exception;
 
-public class BadRequestException {
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
 }
