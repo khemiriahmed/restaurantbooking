1863b3a50be58dc304824e458152d6e7ca4de6eb
khemiriahmed <khemiriahmed6@gmail.com>
2026-09-15 10:44:11 +0100

    Add message

--- backend/src/main/java/com/akhm/restaurantbooking/exception/ResourceNotFoundException.java
+++ backend/src/main/java/com/akhm/restaurantbooking/exception/ResourceNotFoundException.java
@@ -3,6 +3,6 @@ package com.akhm.restaurantbooking.exception;
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String message) {
-       
+       super(message);
     }
 }
