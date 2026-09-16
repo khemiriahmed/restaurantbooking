5cdbf2d39dcc236063e0bc8eebd7b39b55c218b5
khemiriahmed <khemiriahmed6@gmail.com>
2026-09-16 15:25:03 +0100

    Global Exception Handler

--- backend/src/main/java/com/akhm/restaurantbooking/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/akhm/restaurantbooking/exception/GlobalExceptionHandler.java
@@ -2,10 +2,12 @@ package com.akhm.restaurantbooking.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -49,6 +51,32 @@ public class GlobalExceptionHandler {
                 .body(Map.of(
                         "status", 409,
                         "message", exception.getMessage(),
+                        "timestamp", LocalDateTime.now()
+                ));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidation(
+            MethodArgumentNotValidException exception
+    ) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        )
+                );
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of(
+                        "status", 400,
+                        "message", "Validation failed",
+                        "errors", errors,
                         "timestamp", LocalDateTime.now()
                 ));
     }
