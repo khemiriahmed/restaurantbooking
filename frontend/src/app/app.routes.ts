eed8d333452543416aacb025930e9536d00cde70
khemiriahmed <khemiriahmed6@gmail.com>
2026-09-23 19:59:27 +0100

    Implementation home page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+ 
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
