b0805a7bf4200bec798c9f1f11f9875419ce1661
khemiriahmed <khemiriahmed6@gmail.com>
2026-09-23 20:16:07 +0100

    Fix bug component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.component.css'
 })
 export class App {
   protected readonly title = signal('restaurant-booking-frontend');
